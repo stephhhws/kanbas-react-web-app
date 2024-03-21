46a7b517ec040ea563dc41ccd3a270bc7abb5344
stephhhws <stepskycc@gmail.com>
2024-03-20 20:24:46 -0400

    lab4

---
+++ src/Labs/a4/index.tsx
@@ -0,0 +1,38 @@
+import PassingFunctions from "./PassingFunctions";
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
+import EventObject from "./EventObject";
+import Counter from "./Counter";
+import BooleanStateVariables from "./BooleanStateVariables";
+import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
+import ObjectStateVariable from "./ObjectStateVariable";
+import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
+import ChildStateComponent from "./ChildStateComponent";
+import ReduxExamples from "./ReduxExamples";
+
+function Assignment4() {
+  function sayHello() {
+    alert("Hello");
+  }
+  return (
+    <div>
+      <h1>Assignment 4</h1>
+      <ClickEvent />
+      <PassingDataOnEvent />
+      <PassingFunctions theFunction={sayHello} />
+      <EventObject />
+      <Counter />
+      <BooleanStateVariables />
+      <StringStateVariables />
+      <DateStateVariable />
+      <ObjectStateVariable />
+      <ArrayStateVariable />
+      <ParentStateComponent />
+      <ReduxExamples/>
+
+    </div>
+  );
+}
+export default Assignment4;
